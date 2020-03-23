71e2150bc6fc8e6b40f0e6cd13f2205e757ca95e
sanjiv kumar <ksanjiv0005@gmail.com>
2020-03-23 17:54:38 +0530

    add link  into the index

--- MVC-Basic/App_Start/RouteConfig.cs
+++ MVC-Basic/App_Start/RouteConfig.cs
@@ -11,7 +11,7 @@ namespace MVC_Basic
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");//what is this
 
             routes.MapRoute(
                 name: "Default",
