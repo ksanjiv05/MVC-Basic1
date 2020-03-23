6e529155bbe5827f6fedb6f91743f260761c355e
sanjiv kumar <ksanjiv0005@gmail.com>
2020-03-23 12:47:18 +0530

    modify the comic model

--- MVC-Basic/Models/ComicBook.cs
+++ MVC-Basic/Models/ComicBook.cs
@@ -9,10 +9,23 @@ namespace MVC_Basic.Models
     {
 
         public int Id { get; set; }
-        public string IssueTitle { get; set; }
+        public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string description { get; set; }
         public Artist[] Artist { get; set; }
         public bool Favourit { get; set; }
+
+        public string DisplayText
+        {
+            get {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string FileName {
+            get {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
