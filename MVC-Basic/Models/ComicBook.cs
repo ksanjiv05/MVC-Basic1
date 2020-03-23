0f31edb1fa86f29aaaefff657c293887f27cf72b
sanjiv kumar <ksanjiv0005@gmail.com>
2020-03-23 12:39:37 +0530

    Added Comic book model

---
+++ MVC-Basic/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Basic.Models
+{
+    public class ComicBook
+    {
+
+        public int Id { get; set; }
+        public string IssueTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string description { get; set; }
+        public Artist[] Artist { get; set; }
+        public bool Favourit { get; set; }
+    }
+}
